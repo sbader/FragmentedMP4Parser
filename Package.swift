23a71e902c2f28125e32b4e0c3421f3ab42ac08b
Scott Bader <sb@scottbader.org>
2017-06-10 19:32:05 -0400

    Using git protocol

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "FragmentedMP4Parser",
     dependencies: [
-      .Package(url: "https://github.com/sbader/FragmentedMP4Description.git", majorVersion: 0)
+      .Package(url: "git@github.com:sbader/FragmentedMP4Description.git", majorVersion: 0)
     ]
 )
