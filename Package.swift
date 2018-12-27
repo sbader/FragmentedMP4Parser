f25f4673428ec65e6d16c45c94efb6d6b0d35c02
Scott Bader <sb@scottbader.org>
2018-12-27 14:37:01 -0500

    Updates for swift pm 4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,18 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "FragmentedMP4Parser",
+    products: [
+      .library(name: "FragmentedMP4Parser", targets: ["FragmentedMP4Parser"]),
+    ],
     dependencies: [
-      .Package(url: "git@github.com:sbader/FragmentedMP4Description.git", majorVersion: 0)
+      .package(url: "../FragmentedMP4Description", .branch("master"))
+    ],
+    targets: [
+        .target(name: "FragmentedMP4Parser", dependencies: ["FragmentedMP4Description"]),
+        .testTarget(name: "FragmentedMP4ParserTests", dependencies: ["FragmentedMP4Parser"])
     ]
 )
